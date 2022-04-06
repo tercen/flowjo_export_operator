95371d22ae619c47f8207f71b0be453d742daec9
ginberg <info@gerinberg.com>
2022-04-06 09:23:41 +0000

    use cluster as a column

--- workspace.R
+++ workspace.R
@@ -10,13 +10,26 @@ library(dplyr, warn.conflicts = FALSE)
 
 ctx = tercenCtx()
 
+rows <- ctx$rselect()
+if (length(rows) != 1) {
+  stop("There should be a single row containing the rowId")
+}
+
+clusters <- ctx$cselect()
+if (length(clusters) != 1) {
+  stop("There should be a single column for the clusters")
+}
+
+clusters <- cbind(clusters, .ci = seq(0, nrow(clusters) - 1))
+
 ctx %>%
   select(.y, .ci, .ri) %>%
   group_by(.ri, .ci) %>%
   summarise(.y = mean(.y)) %>%
   ungroup() %>%
   cbind(ctx$rselect()) %>%
-  mutate(cluster = .ci+1) %>%
+  left_join(clusters) %>%
+  mutate(cluster = as.integer(gsub("c", "", .[[5]]))) %>%
   select(.ri, cluster) %>%
   ctx$addNamespace() %>%
   ctx$save()
