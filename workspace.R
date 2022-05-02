262d5bc289462e35d5cf7e518a7ad253dcf2b625
ginberg <info@gerinberg.com>
2022-05-02 08:47:10 +0000

    add properties to set NA values

--- workspace.R
+++ workspace.R
@@ -9,14 +9,16 @@ library(dplyr, warn.conflicts = FALSE)
 #options("tercen.password"= "admin")
 
 ctx = tercenCtx()
-na_cluster_value <- -1
 
 rows <- ctx$rselect()
 if (length(rows) <= 2) {
   stop("There should be at least three rows: filename, rowId and a result")
 }
 
-output_folder <- ifelse(is.null(ctx$op.value('output_folder')), "exporting data", as.character(ctx$op.value('output_folder')))
+output_folder    <- ifelse(is.null(ctx$op.value('output_folder')), "exporting data", as.character(ctx$op.value('output_folder')))
+cluster_na_value <- ifelse(is.null(ctx$op.value('cluster_na_value')), 0, as.character(ctx$op.value('cluster_na_value')))
+other_na_value   <- ifelse(is.null(ctx$op.value('other_na_value')), 0, as.character(ctx$op.value('other_na_value')))
+
 
 upload_data <- function(df, folder, file_name, project, client) {
   fileDoc = FileDocument$new()
@@ -69,9 +71,9 @@ get_numeric_value <- function(value) {
   cl <- class(value)
   if (cl == "character") {
     result <- as.numeric(regmatches(value, gregexpr("[[:digit:]]+", value)))
-    result[is.na(result)] <- na_cluster_value
+    result[is.na(result)] <- cluster_na_value
   } else if (cl == "numeric") {
-    value[is.nan(value)] <- na_cluster_value
+    value[is.nan(value)] <- other_na_value
     result <- value
   }
   result
