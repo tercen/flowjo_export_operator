b1a00aed09d5b25a7bcea2631935ccf41bcc21ec
ginberg <info@gerinberg.com>
2022-06-13 10:56:36 +0000

    allow for derived factor filename

--- main.R
+++ main.R
@@ -12,9 +12,11 @@ rows <- ctx$rselect()
 if (length(rows) <= 2) {
   stop("There should be at least three rows: filename, rowId and a result")
 }
-if(!any(grepl("filename", colnames(rows)))) stop("filename is required.")
+filename_in_colnames <- grepl("filename", colnames(rows))
+if(!any(filename_in_colnames)) stop("filename is required.")
 if(!any(grepl("rowId", colnames(rows))))    stop("rowId is required.")
 
+filename_col       <- colnames(rows)[filename_in_colnames]
 output_folder      <- ctx$op.value('output_folder', as.character, "Exported data")
 character_na_value <- ctx$op.value('character_na_value', as.numeric, 0)
 integer_na_value   <- ctx$op.value('integer_na_value', as.numeric, 0)
@@ -27,11 +29,11 @@ if (output_folder != "") {
   folder  <- ctx$client$folderService$getOrCreate(project$id, output_folder)
 }
 
-do.upload <- function(df_tmp, folder, project, ctx) {
-  filename <- df_tmp$filename[1]
-  df_tmp <- select(df_tmp, -filename)
+do.upload <- function(df_tmp, folder, project, ctx, filename_col) {
+  filename  <- df_tmp[[filename_col]][1]
+  df_tmp    <- select(df_tmp, -!!filename_col)
   col_names <- colnames(df_tmp)
-  out_name <- paste(unique(unlist(lapply(
+  out_name  <- paste(unique(unlist(lapply(
     col_names,
     FUN = function(x) { substr(unlist(strsplit(x, "\\."))[2], 1, 11) }
   ))), collapse = "_")
@@ -46,8 +48,8 @@ rows %>%
   mutate_if(is.integer,   ~replace_na(., as.integer(integer_na_value)))   %>%
   mutate_if(is.double,    ~replace_na(., double_na_value))    %>%
   mutate_if(is.character, ~replace_na(., character_na_value)) %>% 
-  group_by(filename) %>% 
-  do(do.upload(., folder, project, ctx)) %>% 
+  group_by_at(filename_col) %>%
+  do(do.upload(., folder, project, ctx, filename_col)) %>%
   ungroup() %>%
   mutate(.ri = seq(0, nrow(.) - 1)) %>%
   ctx$addNamespace() %>%
